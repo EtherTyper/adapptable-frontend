4488541740ff2fded59b0de17d6a2465bb09ff77
Eli Joesph Bradley <elijos03@gmail.com>
2017-03-29 15:42:40 -0500

    Minor changes

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const BrowserWindow = electron.BrowserWindow;  // Module to create native browse
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null;
+let mainWindow = null;
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
@@ -27,7 +27,7 @@ var newWin=function() {
     // and load the index.html of the app.
     mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function() {
